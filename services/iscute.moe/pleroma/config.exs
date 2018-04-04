cae3459deb270526ce93de9b00278c455792ee34
RX14 <chris@rx14.co.uk>
2018-04-04 23:31:14 +0100

    Deploy pleroma on iscute.moe

---
+++ services/iscute.moe/pleroma/config.exs
@@ -0,0 +1,33 @@
+use Mix.Config
+
+config :pleroma, Pleroma.Upload,
+  uploads: "/uploads"
+
+config :pleroma, Pleroma.Web.Endpoint,
+  http: [port: 4000],
+  protocol: "http",
+  url: [host: "iscute.moe", scheme: "https", port: 443]
+
+config :pleroma, :instance,
+  name: "iscute.moe",
+  email: "chris@rx14.co.uk",
+  limit: 5000,
+  registrations_open: true
+
+config :pleroma, :media_proxy,
+  enabled: true,
+  redirect_on_failure: true
+
+config :pleroma, Pleroma.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  hostname: "pleroma_db",
+  username: "pleroma",
+  database: "pleroma_prod",
+  pool_size: 10
+
+config :logger,
+  level: :info
+
+config :tzdata, :data_dir, "/tmp/elixir_tzdata_data"
+
+import_config "prod.secret.exs"
